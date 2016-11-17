fdacfc79e3f1d2085c4e4303bd9f68adf350a325
Munish Satia <munish.satia@gmail.com>
2016-11-17 10:18:32 -0500

    NG Forms Samples

---
+++ ng2forms/src/app/app.complexform.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import {FormBuilder,FormGroup} from '@angular/forms';
+
+@Component({
+    selector : 'complex-form',
+    templateUrl : './app.complexform.html'
+})
+// export class ComplexFormComponent{
+// complexForm : FormGroup;
+
+// constructor(fb:FormBuilder){
+//             this.complexForm = fb.group({
+//                 'firstName' : '',
+//                 'lastname' : '',
+//                 'gender' : 'Female',
+//             'hiking' : false,
+//             'running' : false,
+//             'swimming' : false
+//             })
+//         }
+
+// submitForm(value:any){
+//     console.log('Reactive Form Data');
+//     console.log(value);
+// }
+
+// }
+export class ComplexFormComponent {
+  // The FormGroup object as you may remember from the simple form example exposes various API’s for dealing with forms. Here we are creating a new object and setting its type to FormGroup
+  complexForm : FormGroup;
+
+  // We are passing an instance of the FormBuilder to our constructor
+  constructor(fb: FormBuilder){
+    // Here we are using the FormBuilder to build out our form.
+    this.complexForm = fb.group({
+      // We can set default values by passing in the corresponding value or leave blank if we wish to not set the value. For our example, we’ll default the gender to female.
+      'firstName' : '',
+      'lastName': '',
+      'gender' : 'Female',
+      'hiking' : false,
+      'running' : false,
+      'swimming' : false
+    })
+  }
+
+  // Again we’ll implement our form submit function that will just console.log the results of our form
+  submitForm(value: any):void{
+    console.log('Reactive Form Data: ')
+    console.log(value);
+  }
+}
